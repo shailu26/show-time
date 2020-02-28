a86d396fc92ec397f9c5738df43fdf3dbdcf547b
shailesh2601 <shailesh@flowace.in>
2020-02-28 20:45:01 +0530

    changed backend port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./application');
-const port = process.env.NODE_ENV === 'development' ? 5001 : 4000;
+const port = process.env.NODE_ENV === 'development' ? 5002 : 4000;
 const server = app.listen(port,()=>{
     console.log(`server is running on ${port}`)
 })
